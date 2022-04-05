9b5938cdd9fb6b178d8115914c7ddf8cac6fbc08
Gunjan Solanki <gunjan.solanki@bacancy.com>
2022-04-05 19:56:48 +0530

    Add product to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+niceCat = Product.create(title: "Quality Kitty",
+  subtitle: "Learn App code", 
+  image_name: "kitty.JPG",
+  price: "4.99", sku: "kittyOne", download_url: "https://rails-learn-app-bucket.s3.ap-south-1.amazonaws.com/dell-2.jpg",
+  details: "A really nice Cat", description: %{<p>Its a cat</p>}
+)
+
+niceDoggo = Product.create(title: "Quality Doggo",
+  subtitle: "Learn App code", 
+  image_name: "doggo.JPG",
+  price: "4.99", sku: "kittyOne", download_url: "https://rails-learn-app-bucket.s3.ap-south-1.amazonaws.com/dell-2.jpg",
+  details: "A really nice Doggo", description: %{<p>Its a doggo</p>}
+)
